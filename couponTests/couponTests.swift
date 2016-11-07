743092249571c72c9cce66b93be4dd92b5c380cc
Alexander Murphy <amurphy345@gmail.com>
2016-11-07 11:24:28 -0700

    test case working

--- couponTests/couponTests.swift
+++ couponTests/couponTests.swift
@@ -7,13 +7,21 @@
 //
 
 import XCTest
+import RealmSwift
 @testable import coupon
 
 class couponTests: XCTestCase {
+    var processedRetailers: [Object]?
+    var processedOffers: [Object]?
+    var processedLocations: [Object]?
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        //load retailers, offers and locations into Realm with associations
+        processedRetailers = RetailerTranslatorHelper.translateRetailer(data: ReadJSONHelper.getAllRetailers()!)
+        processedOffers = OfferTranslatorHelper.translateOffer(data: ReadJSONHelper.getAllOffers()!)
+        processedLocations = LocationTranslatorHelper.translateLocation(data: ReadJSONHelper.getAllLocaitons()!)
     }
     
     override func tearDown() {
@@ -24,6 +32,13 @@ class couponTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let retailerOne = processedRetailers?[0]["name"] as! String
+        let offerOne = processedOffers?[0]["name"] as! String
+        let locationOne = processedLocations?[0]["id"] as! Int
+        
+        XCTAssertEqual(retailerOne, "H-E-B")
+        XCTAssertEqual(offerOne, "RushÂ® Bowls")
+        XCTAssertEqual(locationOne, 22920)
     }
     
     func testPerformanceExample() {
